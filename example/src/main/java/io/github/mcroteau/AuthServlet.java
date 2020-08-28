c2627b9923bba921a1c40227a7549b75023168c9
Mike Croteau <croteau.mike@gmail.com>
2020-08-28 11:47:12 -0800

    basics servlet started

---
+++ example/src/main/java/io/github/mcroteau/AuthServlet.java
@@ -0,0 +1,33 @@
+package io.github.mcroteau;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class AuthServlet extends HttpServlet {
+
+    ParakeetFactory parakeetFactory;
+
+    public AuthServlet(){
+        parakeetFactory = new ParakeetFactory();
+    }
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello</h1>");
+    }
+
+
+}
