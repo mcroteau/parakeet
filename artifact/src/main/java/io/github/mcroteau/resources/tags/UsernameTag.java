ee2c9a2cc39d86b8c7a91d68bf38ef8407db59c8
Mike Croteau <croteau.mike@gmail.com>
2020-09-03 19:22:40 -0800

    taglib effort

--- artifact/src/main/java/io/github/mcroteau/resources/tags/UsernameTag.java
+++ artifact/src/main/java/io/github/mcroteau/resources/tags/UsernameTag.java
@@ -25,11 +25,11 @@ public class UsernameTag extends TagSupport {
 
             if(session != null) {
                 ServletContext context = req.getServletContext();
-                Accessor accessor = (Accessor) context.getAttribute(Constants.ACCESSOR_LOOKUP);
+                Parakeet parakeet = (Parakeet) context.getAttribute(Constants.PARAKEET_LOOKUP);
 
-                if(accessor == null) return TagSupport.SKIP_BODY;
+                System.out.println("username parakeet : " + parakeet);
 
-                Parakeet parakeet = new Parakeet(accessor);
+                if(parakeet == null) return TagSupport.SKIP_BODY;
 
                 if(parakeet.isAuthenticated()){
                     out.println(parakeet.getUser());
